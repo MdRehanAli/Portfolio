502bf7283c2a744c60d1c5025c0a53b6b25d32a8
MdRehanAli <rehanali21d@gmail.com>
2024-04-15 14:13:35 +0600

    added index files and styles

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import routes from './routes/routes.js';
+import path from 'path'
 const app = express();
 
 const port = process.env.PORT || 8080
 
+//setup for static files
+app.use(express.static(path.join(process.cwd(), 'public')))
+
+// ejs setup
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
 //create routes
-app.get('/', (req, res) => {
-    res.send("Welcome to our website")
-})
+app.use('/', routes)
 
 app.listen(port, () => {
     console.log("server is running port ${port}")
